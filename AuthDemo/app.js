89a42d5a181eafe557113b8521bdc9f51d6c902d
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-19 13:08:20 +0000

    add secret route

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -7,6 +7,10 @@ app.get("/", function(req, res){
     res.render("home");
 })
 
+app.get("/secret", function(req, res){
+    res.render("secret");
+})
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started.");
 })
