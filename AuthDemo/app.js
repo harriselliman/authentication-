275c9b7189e14340503d73f3f0b9963a9dd23cdd
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-19 13:15:21 +0000

    require all packages

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,5 +1,10 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParser              = require("body-parser"),
+    LocalStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose");
+
 mongoose.connect("mongodb://localhost:27017/auth_demo_app", { useNewUrlParser: true });
 
 var app = express();
