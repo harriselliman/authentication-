b1385723a9b3804981230887efae56469f10c39d
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-19 13:20:11 +0000

    require user model

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -2,6 +2,7 @@ var express                 = require("express"),
     mongoose                = require("mongoose"),
     passport                = require("passport"),
     bodyParser              = require("body-parser"),
+    User                    = require("./models/user"),
     LocalStrategy           = require("passport-local"),
     passportLocalMongoose   = require("passport-local-mongoose");
 
@@ -12,11 +13,11 @@ app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("home");
-})
+});
 
 app.get("/secret", function(req, res){
     res.render("secret");
-})
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started.");
