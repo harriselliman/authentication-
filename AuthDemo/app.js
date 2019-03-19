442f7e157880b879bed30fd00f8c1beeebd1bc62
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-19 13:12:38 +0000

    connect to mongoose

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/auth_demo_app", { useNewUrlParser: true });
 
 var app = express();
 app.set("view engine", "ejs");
