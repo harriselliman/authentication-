a79a167935d30253956052efa77e3d153452a21f
Harris Elliman <harris.elliman@hotmail.co.uk>
2019-03-19 13:06:18 +0000

    add app.listen

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -6,3 +6,7 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.render("home");
 })
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started.");
+})
